7e6b8f828f8503e42a187c36d8a41a84632972e8
huge man <hugeman@huge-manmatoiMac.local>
2012-06-11 11:40:09 +0800

    Initial Commit

---
+++ WaterFlowViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WaterFlowViewDemo
+//
+//  Created by man huge on 12-6-11.
+//  Copyright (c) 2012年 activation group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WaterFlowViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WaterFlowViewDemo
+//
+//  Created by man huge on 12-6-11.
+//  Copyright (c) 2012年 activation group. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
